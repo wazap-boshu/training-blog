9f9f0a7f7b3572670ac845a23d18e3fd45c73ee1
イワブチフミヤ <buchi0326iwa@gmail.com>
2024-04-14 19:31:06 +0900

    テストapiを作成

---
+++ training-blog/src/app/api/test/[is]/route.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/app/libs/prisma"
+import { NextRequest, NextResponse } from "../../../../node_modules/next/server";
+import { getServerSession } from "next-auth/next";
+import { options } from "@/app/options";
+
+export async function GET(request: Request) {
+  try {
+    return NextResponse.json({ message: "TESTSUCCEEDED" });
+  } catch {
+    return NextResponse.error()
+  }
+}
