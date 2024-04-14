e600cbc4bbe1eacb8999464ecc0d2b5b9283cf30
イワブチフミヤ <buchi0326iwa@gmail.com>
2024-04-14 21:28:31 +0900

    Errorが表示されるようにした

--- training-blog/src/app/api/posts/[id]/route.ts
+++ training-blog/src/app/api/posts/[id]/route.ts
@@ -7,12 +7,19 @@ export async function GET(request: Request, { params }: { params: { id: string }
   try {
     const id = params.id;
 
-    const posts = await prisma.post.findMany()
+    const post = await prisma.post.findUnique({
+      where: {
+        id: id
+      }
+    })
 
-    const post = posts[0]
-
-    return NextResponse.json([post]);
-  } catch {
-    return NextResponse.error();
+    return NextResponse.json({
+      id: "testid",
+      title: "タイトル",
+      content: "これはこれです",
+      date: new Date(),
+    });
+  } catch (error) {
+    return NextResponse.json(error);
   }
 }
